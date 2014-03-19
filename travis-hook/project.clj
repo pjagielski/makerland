2077617653354a358df3c86fd109f8395cd9970f
Piotr Jagielski <pjg@touk.pl>
2014-03-19 12:22:18 +0100

    uberjar fix

--- travis-hook/project.clj
+++ travis-hook/project.clj
@@ -4,7 +4,8 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]
                  [ring/ring-core "1.2.0"]
-                 [ring/ring-json "0.1.2"]]
+                 [ring/ring-json "0.1.2"]
+                 [ring/ring-jetty-adapter "1.2.0"]]
 
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler travis-hook.handler/app}
